ca151c3e8477944d092b093b661c53f58fb09342
VGravius <vg.gravius@gmail.com>
2026-06-19 16:41:23 +0300

    Реализация gRPC сервиса

---
+++ gRPC-Service.cpp
@@ -0,0 +1,12 @@
+﻿// gRPC-Service.cpp: определяет точку входа для приложения.
+//
+
+#include "gRPC-Service.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
